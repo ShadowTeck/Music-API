5c4205529471898e7818a3ba2587d1210731e620
Liam <shadowteckst@gmail.com>
2021-11-22 14:10:08 -0700

    fsjdflskjdflksd help me

--- controllers/Playlist.js
+++ controllers/Playlist.js
@@ -11,36 +11,34 @@ const getAllPlaylists = async (req, res) => {
 const getPlaylist = async (req, res) => {
   //Gets one playlist from user
 
-  res.send(`working`);
+  res.json(`working`);
 };
 
 const createPlaylist = async (req, res) => {
-  //creates/adds playlist to playlists
-  req.body.createdBy = req.user.userID;
-  const playlist = await Playlist.create(req.body);
-
-  res.status(StatusCodes.CREATED).json({ playlist });
-  res.json({status: 200, results: 'success'})
+    req.body.createdBy = req.user.userID;
+    const job = await Job.create(req.body)
+    res.status(StatusCodes.CREATED).json({job})
+    res.json({status: 200, results: 'success'})
 };
 
 const updatePlaylist = async (req, res) => {
   //Updates a playlist by either changing the name of it, adding a song, or deleting a song
-  res.send(`working`);
+  res.json(`working`);
 };
 
 const deleteSong = async (req, res) => {
   //deletes specified song
-  res.send(`working`);
+  res.json(`working`);
 };
 
 const addSong = async (req, res) => {
   //adds specified song
-  res.send(`working`);
+  res.json(`working`);
 };
 
 const editSong = async (req, res) => {
     //edits specified song
-    res.send(`working`)
+    res.json(`working`)
 }
 
 module.exports = {addSong, deleteSong, editSong, updatePlaylist, createPlaylist, getPlaylist, getAllPlaylists}
